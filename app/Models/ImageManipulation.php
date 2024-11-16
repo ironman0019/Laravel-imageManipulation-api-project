93531244011dc50fea226651b204981f76c5ff5d
Emad Isavi <emadesavi@gmail.com>
2024-11-17 02:50:36 +0330

    Add fillable and fix some settings

--- app/Models/ImageManipulation.php
+++ app/Models/ImageManipulation.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class ImageManipulation extends Model
 {
-    //
+    const TYPE_RESIZE = 'resize';
+
+    const UPDATED_AT = null;
+
+    protected $fillable = ['name', 'path', 'type', 'data', 'output_path', 'user_id', 'album_id'];
 }
