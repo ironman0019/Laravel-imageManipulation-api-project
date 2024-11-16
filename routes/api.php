b161aa7c697205a476e0fd0068f9146cd4d714c4
Emad Isavi <emadesavi@gmail.com>
2024-11-17 02:51:34 +0330

    Add image manipulation routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\V1\AlbumController;
+use App\Http\Controllers\V1\ImageManipulationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,4 +12,9 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 
 Route::prefix('v1')->group(function() {
     Route::apiResource('album', AlbumController::class);
+    Route::get('image', [ImageManipulationController::class, 'index']);
+    Route::get('image/{image}', [ImageManipulationController::class, 'show']);
+    Route::get('image/by-album/{album}', [ImageManipulationController::class, 'byAlbum']);
+    Route::post('image/resize', [ImageManipulationController::class, 'resize']);
+    Route::delete('image/{image}', [ImageManipulationController::class, 'destroy']);
 });
