faa12e6d6112f057ed46c80b3649e0435a2c1064
Emad Isavi <emadesavi@gmail.com>
2024-11-16 20:34:14 +0330

    Create image manipulation model & migration

---
+++ database/migrations/2024_11_16_180640_create_image_manipulations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('image_manipulations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('path', 2000);
+            $table->string('type', 25);
+            $table->text('data');
+            $table->string('output_path', 2000)->nullable();
+            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('album_id')->nullable()->constrained('albums')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('image_manipulations');
+    }
+};
