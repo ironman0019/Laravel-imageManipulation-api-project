fb9780a358703a5a29ba3a629773aa8e01c331c9
Emad Isavi <emadesavi@gmail.com>
2024-11-16 20:33:03 +0330

    Add album api resource route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\V1\AlbumController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::prefix('v1')->group(function() {
+    Route::apiResource('album', AlbumController::class);
 });
