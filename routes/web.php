1cfc33f26137d2770a14a93526f6a357bbbdb7a7
Emad Isavi <emadesavi@gmail.com>
2024-11-18 02:33:29 +0330

    Add dashboard routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +16,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    // Dashboard routes
+    Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
+    Route::get('/token/create', [DashboardController::class, 'showTokenForm'])->name('token.showForm');
+    Route::post('/token/create', [DashboardController::class, 'createToken'])->name('token.create');
+    Route::post('/token/delete/{token}', [DashboardController::class, 'deleteToken'])->name('token.delete');
 });
 
 require __DIR__.'/auth.php';
